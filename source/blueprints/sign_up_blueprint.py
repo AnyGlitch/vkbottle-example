1071294977d96ef64a045bd2459d76aaef2cb354
Any Glitch <WhyGlitches@gmail.com>
2022-03-30 08:05:55 +0300

    Fix handlers

--- source/blueprints/sign_up_blueprint.py
+++ source/blueprints/sign_up_blueprint.py
@@ -1,4 +1,5 @@
 from vkbottle.bot import Blueprint, Message
+from vkbottle.dispatch.rules.base import CommandRule, PayloadRule
 
 from source.database.models import UserModel
 from source.keyboards import HOME_KEYBOARD
@@ -6,8 +7,7 @@ from source.keyboards import HOME_KEYBOARD
 bp = Blueprint("User sign up")
 
 
-@bp.on.private_message(command="start")
-@bp.on.private_message(payload={"command": "start"})
+@bp.on.private_message(CommandRule("start") | PayloadRule({"command": "start"}))
 async def sign_up_handler(message: Message, user_exists: bool) -> str | dict:
     if user_exists:
         return "You have already been previously registered..."
