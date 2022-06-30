f94aa81e0541c72b1126596cf0fb4a84db0f063e
Any Glitch <WhyGlitches@gmail.com>
2022-06-30 19:55:37 +0300

    Bring to standard rules

--- source/blueprints/about_blueprint.py
+++ source/blueprints/about_blueprint.py
@@ -1,4 +1,5 @@
 from vkbottle.bot import Blueprint, Message
+from vkbottle.dispatch.rules.base import PayloadRule
 
 from source.keyboards import ABOUT_PROJECT_KEYBOARD
 from source.messages import ABOUT_PROJECT_MESSAGE
@@ -6,7 +7,7 @@ from source.messages import ABOUT_PROJECT_MESSAGE
 bp = Blueprint("About many things")
 
 
-@bp.on.private_message(payload={"command": "about_project"})
+@bp.on.private_message(PayloadRule({"command": "about_project"}))
 async def about_project_handler(_: Message) -> dict:
     return {
         "message": ABOUT_PROJECT_MESSAGE,
--- source/blueprints/back_blueprint.py
+++ source/blueprints/back_blueprint.py
@@ -1,4 +1,5 @@
 from vkbottle.bot import Blueprint, Message
+from vkbottle.dispatch.rules.base import PayloadRule
 
 from source.keyboards import HOME_KEYBOARD
 from source.messages import BACK_TO_HOME_MESSAGE
@@ -6,6 +7,6 @@ from source.messages import BACK_TO_HOME_MESSAGE
 bp = Blueprint("Back to keyboards")
 
 
-@bp.on.private_message(payload={"command": "back_to_home"})
+@bp.on.private_message(PayloadRule({"command": "back_to_home"}))
 async def back_to_home_handler(_: Message) -> dict:
     return {"message": BACK_TO_HOME_MESSAGE, "keyboard": HOME_KEYBOARD}
